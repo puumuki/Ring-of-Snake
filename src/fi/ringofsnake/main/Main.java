cf5d4651305d5d6e50c7d3b60d1f9dc1d9a1b7a5
Teemu Puukko <teemuki@gmail.com>
2012-01-28 00:05:54 +0200

    Renamed packets

---
+++ src/fi/ringofsnake/main/Main.java
@@ -0,0 +1,18 @@
+package fi.ringofsnake.main;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Main extends StateBasedGame {
+
+	public Main(String name) {
+		super(name); 
+	}
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		
+	}
+
+}
