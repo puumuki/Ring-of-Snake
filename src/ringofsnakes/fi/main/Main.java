08756d4477461f88d97a7e6f32520c20d50863f9
Teemu Puukko <teemuki@gmail.com>
2012-01-27 23:47:45 +0200

    Initial commit of Ring Of Snake

---
+++ src/ringofsnakes/fi/main/Main.java
@@ -0,0 +1,18 @@
+package ringofsnakes.fi.main;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Main extends StateBasedGame {
+
+	public Main(String name) {
+		super(name); 
+	}
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		
+	}
+
+}
