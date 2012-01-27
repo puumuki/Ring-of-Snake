b228713f3deee29a3109e352108b1c97df5b9121
Miika Hamynen <yaamboo@gmail.com>
2012-01-28 01:35:06 +0200

    gamestates updated

--- src/fi/ringofsnake/gamestates/MainMenuGameState.java
+++ src/fi/ringofsnake/gamestates/MainMenuGameState.java
@@ -1,5 +1,42 @@
 package fi.ringofsnake.gamestates;
 
-public class MainMenuGameState {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainMenuGameState extends BasicGameState {
+
+		private int stateID = -1;
+		
+		public MainMenuGameState(int stateID) {
+			this.stateID = stateID;
+		}
+		@Override
+		public void init(GameContainer container, StateBasedGame game)
+				throws SlickException {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void render(GameContainer container, StateBasedGame game, Graphics g)
+				throws SlickException {
+			// TODO Auto-generated method stub
+
+			g.drawString("it works!", 10, 10);
+			
+		}
+		@Override
+		public void update(GameContainer container, StateBasedGame game, int delta)
+				throws SlickException {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public int getID() {
+			// TODO Auto-generated method stub
+			return stateID;
+		}
 
 }
